6891e86c7add3867c31a33cde6e542560de86b40
Vinnie Dolan <vpdolan@gmail.com>
2015-12-07 10:20:29 -0500

    updating user files

--- app/js/main.js
+++ app/js/main.js
@@ -495,6 +495,26 @@ var PetRegService = function PetRegService($http, SERVER, $cookies, $state) {
 PetRegService.$inject = ['$http', 'SERVER', '$cookies', '$state'];
 
 exports['default'] = PetRegService;
+
+//to upload pet photos by owner
+// let UploadService = function($http, FILESERVER) {
+
+//   this.upload = upload;
+
+//   function upload (file) {
+
+//     let formData = new FormData();
+//     formData.append('upload', file);
+//     // formData.append('details', JSON.stringify({ name: 'Tim' }));
+
+//     return $http.post(FILESERVER.URL, formData, FILESERVER.CONFIG);
+//   }
+
+// };
+
+// UploadService.$inject = ['$http', 'FILESERVER'];
+
+// export default UploadService;
 module.exports = exports['default'];
 
 },{}],14:[function(require,module,exports){
